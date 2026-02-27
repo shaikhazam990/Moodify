43e2d82915695948e1a90ca96a1506a79e7a2c3e
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-27 13:29:11 +0530

    .

---
+++ Backend/src/app.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const cookieparser = require("cookie-parser")
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieparser())
+
+module.exports=app
+
