334dac5ab558e06627936ec65605c1b3ecee9b4c
shaikhazam990 <shaikhazam0990@gmail.com>
2026-02-28 15:07:14 +0530

    create user.middleware

--- Backend/src/middlewares/user.middleware.js
+++ Backend/src/middlewares/user.middleware.js
@@ -27,11 +27,15 @@ async function userAuth(req,res,next){
         const decode = jwt.verify(token, process.env.JWT_SECRET)
 
         req.user=decode
+        next()
         
     } catch (error) {
-        
+        return res.status(401).json({
+            message:"Invalid token"
+        })   
     }
+}
 
-
-    
+module.exports={
+    userAuth
 }
